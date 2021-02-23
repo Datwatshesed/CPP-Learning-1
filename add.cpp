bbf49aa38b4bf7e34834037000ed0f072482e8f0
Datwatshesed <fidelloblue@protonmail.com>
2021-02-23 10:58:46 +0800

    Tested what will happen when std::endl is removed.

--- add.cpp
+++ add.cpp
@@ -4,7 +4,7 @@ int main() {
 
     // std::cout << "Enter two numbers: " << std::endl;
     std::cout << "Enter two numbers:";
-    std::cout << std::endl; //flushes the buffert to ensure output is written to the output stream
+    std::cout << std::endl; //flushes the buffer to ensure output is written to the output stream
 
     int v1 = 0, v2 = 0;
 
