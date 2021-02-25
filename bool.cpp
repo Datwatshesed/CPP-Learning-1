37dd3c4fc9145b1c495ae43002da6ba5f56db32c
Datwatshesed <fidelloblue@protonmail.com>
2021-02-25 11:11:59 +0800

    Learned how to evaluate booleans

---
+++ bool.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    bool i_like_pie = true;
+    bool random_number = 5;
+    bool not_true = false;
+    bool also_not_true = 0;
+    bool lets_try_floats = 2.1;
+
+    std::cout << std::boolalpha << i_like_pie << " " << random_number << " " << not_true << " " << 
+    also_not_true << " " << lets_try_floats;
+
+    return 0;
+}
