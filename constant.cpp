8578ce4be2ebc267f44d4c21ba53af88064b1f8d
Datwatshesed <fidelloblue@protonmail.com>
2021-02-25 11:45:51 +0800

    Learned how to use constants

---
+++ constant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+
+    const int number = 5;
+
+    cout << "number is a constant containing the number " << number << ". This value can't be changed." << std::endl;
+
+    return 0;
+}
